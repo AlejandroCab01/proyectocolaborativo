c1767260b7edd2189ecd0bd3799ad84f09ec3d03
AlejandroC <alejandrocabreraberna@ieselrincon.es>
2025-01-22 09:16:52 +0100

    función fecha

--- script.sh
+++ script.sh
@@ -3,11 +3,19 @@ echo "script para apender a progamar en bash de manera colaborativa "
 echo "1) Mostrar archivos en el directorio actual "
 echo "2 ) Mostrar la fecha y la hora "
 echo "3) Salir "
-read -p "selecciona opción " opción 
+read -p "selecciona opción " opcion 
 case $opcion
-    1) ls ;;
+    1) listar ;;
     2) date ;;
     3) echo "Adiós " exit ;;
+    4) echo "saludar al usuario"
+    5) echo "opción por desarrollar "
     *) echo "opción no válida"
 
+
     esac
+
+ listar (){
+    ls -l 
+
+ }
